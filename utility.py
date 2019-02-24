35543c568337f3a7f1d32dc4fedc7e8bf476d772
David Carvalho <1160640@isep.ipp.pt>
2019-02-24 19:58:09 +0000

    Slow(er) version of shapesOfN with a dict {tuple: area}

--- utility.py
+++ utility.py
@@ -2,16 +2,22 @@ from math import *
 
 def shapesOfN(H, L):
     #Shapes are stores in (Row, Column) format
-    shapes = list()
+    shapes = dict()
     tempShapes = list()
 
     for i in range(1, ceil(sqrt(H))+1):
         for o in range(1, ceil(sqrt(H))+1):
             if i*o<=H and i*o>=L*2:
-                    shapes.append((i, o))
+                    shapes[(i, o)] = i*o
                     if(i != o):
-                        shapes.append((o, i))
+                        shapes[(i, o)] = i*o
 
+    shapes[(1, H)] = H
+    shapes[(H, 1)] = H
     #ordering shapes by maximum area
+    sortedList = sorted(shapes, key=shapes.get)
+    shapes.clear()
+    for i in sortedList:
+        shapes[i] = i[0]*i[1]
 
     return shapes
