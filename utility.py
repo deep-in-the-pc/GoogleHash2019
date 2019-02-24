f28747a7f7e52eccb0168d9cf9a4dd29409ffdb7
David Carvalho <1160640@isep.ipp.pt>
2019-02-24 19:33:26 +0000

    Fixed shapesOfN

--- utility.py
+++ utility.py
@@ -1,29 +1,17 @@
-#from math import *
+from math import *
 
-def shapesOfN(n):
+def shapesOfN(H, L):
     #Shapes are stores in (Row, Column) format
     shapes = list()
     tempShapes = list()
-    shapes.append((1, n)) # Basic shapes
-    shapes.append((n, 1)) # Vertical and Horizontal Line
 
-    if n%2==0:
-        for i in range(2, n, 2):
-            for o in range(1, n):
-                if i*o<=n:
-                    tempShapes.append((i, o))
-                    tempShapes.append((o, i))
-    else:
-        for i in range(2, n-1, 2):
-            for o in range(1, n-1):
-                if i*o<=n:
-                    tempShapes.append((i, o))
+    for i in range(1, ceil(sqrt(H))+1):
+        for o in range(1, ceil(sqrt(H))+1):
+            if i*o<=H and i*o>=L*2:
+                    shapes.append((i, o))
                     if(i != o):
-                        tempShapes.append((o, i))
+                        shapes.append((o, i))
 
     #ordering shapes by maximum area
-
-    for i in reversed(tempShapes):
-        shapes.append(i)
 
     return shapes
