9170046cbaf28850ca9051c1353e507c1f3cd519
David Carvalho <1160640@isep.ipp.pt>
2019-02-24 20:05:58 +0000

    Fixed shapesOfN (am retarded)

--- utility.py
+++ utility.py
@@ -1,20 +1,18 @@
 from math import *
 
 def shapesOfN(H, L):
-    #Shapes are stores in (Row, Column) format
+    #Shapes are stores in (Row, Column): Area format
     shapes = dict()
     tempShapes = list()
 
     for i in range(1, ceil(sqrt(H))+1):
-        for o in range(1, ceil(sqrt(H))+1):
+        for o in range(1, H+1):
             if i*o<=H and i*o>=L*2:
                     shapes[(i, o)] = i*o
                     if(i != o):
-                        shapes[(i, o)] = i*o
+                        shapes[(o, i)] = i*o
 
-    shapes[(1, H)] = H
-    shapes[(H, 1)] = H
-    #ordering shapes by maximum area
+    #ordering shapes by min to max area
     sortedList = sorted(shapes, key=shapes.get)
     shapes.clear()
     for i in sortedList:
